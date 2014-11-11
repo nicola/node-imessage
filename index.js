a65e9e55b0aa836842db45a2de141dfe09a7adbb
Ben Drucker <bvdrucker@gmail.com>
2014-11-11 09:30:56 -0500

    Refactor API using Knex

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
+'use strict';
+
 var path = require('path');
-var sqlite3 = require('sqlite3').verbose();
-var Q = require('q');
 
 var HOME = getUserHome();
-
-var identities = [];
 
 function getUserHome() {
   var envVar = (process.platform == 'win32') ? 'USERPROFILE' : 'HOME';
@@ -15,50 +13,28 @@ module.exports = iMessage;
 
 function iMessage(opts) {
   opts = opts || {};
-  this.path = opts.path || iMessage.DB_PATH;
-  this.db = this.connect();
+  this.knex = require('knex')({
+    client: 'sqlite',
+    connection: {
+      filename: opts.path || iMessage.DB_PATH,
+      mode: require('sqlite3').OPEN_READONLY
+    }
+  });
 }
 
 iMessage.OSX_EPOCH = 978307200;
 iMessage.DB_PATH = path.join(HOME, '/Library/Messages/chat.db');
 
-iMessage.prototype.connect = function() {
-  var deferred = Q.defer();
-
-  var db = new sqlite3.Database(
-    this.path,
-    sqlite3.OPEN_READONLY,
-    function(err, res) {
-      if (err) return deferred.reject(err);
-      return deferred.resolve(db);
-    });
-
-  return deferred.promise;
-};
-
-iMessage.prototype.getDb = function(cb) {
-  var args = arguments;
-
-  // nodeify?
-  this.db
-    .then(function(db) {
-      cb(null, db);
-    }, function(err) {
-      cb(err);
-    });
-};
-
 iMessage.prototype.getRecipients = function(string, cb) {
-  if (typeof string == 'function') {
+  if (typeof string === 'function') {
     cb = string;
     string = false;
   }
-  this.db.done(function(db) {
-    var where = "";
-    // Maybe dangerous, check SQLlite doc
-    if (string && string != "") where = " WHERE id LIKE '%"+string+"%'";
-    db.all("SELECT * FROM `handle`" + where, cb);
-  });
+  var query = this.knex
+    .select()
+    .from('handle');
+  if (string) query.where('id', 'like', '%' + string + '%');
+  return query.nodeify(cb);
 };
 
 iMessage.prototype.getRecipientById = function(id, details, cb) {
@@ -66,17 +42,23 @@ iMessage.prototype.getRecipientById = function(id, details, cb) {
     cb = details;
     details = false;
   }
-  this.db.done(function(db) {
-    db.get("SELECT * FROM `handle` WHERE ROWID = $id", { $id: id }, function(err, recipient) {
-      if (!details) return cb(err, recipient);
-      if (err) return cb(err);
-      db.all("SELECT * FROM `message` WHERE handle_id = $id", {$id: id}, function(err, messages) {
-        if (err) return cb(err);
-        recipient.messages = messages;
-        cb(err, recipient);
-      });
-    });
-  });
+  return this.knex
+    .first()
+    .from('handle')
+    .where('ROWID', id)
+    .bind(this)
+    .tap(function (recipient) {
+      if (details) {
+        return this.knex
+          .select()
+          .from('message')
+          .where('handle_id', id)
+          .then(function (messages) {
+            recipient.messages = messages;
+          });
+      }
+    })
+    .nodeify(cb);
 };
 
 iMessage.prototype.getMessages = function(string, details, cb) {
@@ -88,15 +70,14 @@ iMessage.prototype.getMessages = function(string, details, cb) {
     cb = details;
     details = false;
   }
+  var query = this.knex
+    .select()
+    .from('message');
 
-  this.db.done(function(db) {
-    var where = "";
-    var join = "";
-    // Maybe dangerous, check SQLlite doc
-    if (string && string != "") where = " WHERE `message`.text LIKE '%"+string+"%'";
-    if (details) join = " JOIN `handle` ON `handle`.ROWID = `message`.handle_id";
-    db.all("SELECT * FROM `message`" + join + where, cb);
-  });
+  if (string) query.where('message.text', 'like', '%' + string + '%');
+  if (details) query.join('handle', 'handle.rowID', 'message.handle_id');
+  
+  return query.nodeify(cb);
 };
 
 iMessage.prototype.getMessagesFromId = function(id, string, cb) {
@@ -104,42 +85,35 @@ iMessage.prototype.getMessagesFromId = function(id, string, cb) {
     cb = string;
     string = false;
   }
+  var query = this.knex
+    .select()
+    .from('message')
+    .where('handle_id', id);
 
-  this.db.done(function(db) {
-    var where = "";
-    // Maybe dangerous, check SQLlite doc
-    if (string && string != "") where = " AND text LIKE '%"+string+"%'";
-    db.all("SELECT * FROM `message` WHERE handle_id = $id"+where, {$id: id}, function(err, messages) {
-      cb(err, messages);
-    });
-  });
+  if (string) query.where('text', 'like', '%' + string + '%');
+  
+  return query.nodeify(cb);
 };
 
 iMessage.prototype.getAttachmentsFromId = function(id, cb) {
-  this.db.done(function(db) {
-    db.all("SELECT * FROM `message` \
-      INNER JOIN `message_attachment_join` \
-      ON `message`.ROWID = `message_attachment_join`.message_id \
-      INNER JOIN `attachment` \
-      ON `attachment`.ROWID = `message_attachment_join`.attachment_id \
-      WHERE `message`.handle_id = $id", {$id: id}, function(err, messages) {
-      cb(err, messages);
-    });
-  });
+  return this.knex
+    .select()
+    .from('message')
+    .innerJoin('message_attachment_join', 'message.ROWID', 'message_attachment_join.message_id')
+    .innerJoin('attachment', 'attachment.ROWID', 'message_attachment_join.attachment_id')
+    .where('message.handle_id', id)
+    .nodeify(cb);
 };
 
 iMessage.prototype.getAttachments = function(cb) {
-  this.db.done(function(db) {
-    db.all("SELECT * FROM `message_attachment_join` \
-      INNER JOIN `message` \
-      ON `message`.ROWID = `message_attachment_join`.message_id \
-      INNER JOIN `attachment` \
-      ON `attachment`.ROWID = `message_attachment_join`.attachment_id", cb);
-  });
+  return this.knex
+    .select()
+    .from('message_attachment_join')
+    .innerJoin('message', 'message.ROWID', 'message_attachment_join.message_id')
+    .innerJoin('attachment', 'attachment.ROWID', 'message_attachment_join.attachment_id')
+    .nodeify(cb);
 };
 
 iMessage.prototype.disconnect = function() {
-  this.db.done(function(db) {
-    db.close();
-  });
+  return this.knex.destroy();
 };
